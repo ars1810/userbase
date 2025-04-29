690817f0732204df7ae518474e335ba2dbd989b4
ars1810 <122894254+ars1810@users.noreply.github.com>
2025-04-29 12:09:48 +0700

    Update database.go

--- backend/config/database.go
+++ backend/config/database.go
@@ -11,12 +11,11 @@ var DB *gorm.DB
 func ConnectDatabase() {
     var err error
 
-    // Ganti dengan connection string PostgreSQL
-    dsn := "host=localhost user=postgres password=1810 dbname=expense_db port=5432 sslmode=disable"
-    DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        log.Fatal("Failed to connect to database:", err)
-    }
-
-    log.Println("Database connected!")
-}
+    dsn := fmt.Sprintf(
+  "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+  os.Getenv("DB_HOST"),
+  os.Getenv("DB_USER"),
+  os.Getenv("DB_PASSWORD"),
+  os.Getenv("DB_NAME"),
+  os.Getenv("DB_PORT"),
+)
