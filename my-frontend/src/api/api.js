5313f451b875ec8c26a88eff85edf316094adfea
ars1810 <122894254+ars1810@users.noreply.github.com>
2025-04-29 13:56:21 +0700

    Update api.js

--- my-frontend/src/api/api.js
+++ my-frontend/src/api/api.js
@@ -1,3 +1,7 @@
+import axios from 'axios';
+
 const API = axios.create({
-  baseURL: 'https://userbase-2.onrender.com/api'
-})
+  baseURL: 'https://userbase-2.onrender.com/api'  // Ini URL backend kamu di Render
+});
+
+export default API;
