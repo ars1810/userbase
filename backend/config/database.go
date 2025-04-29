cc02f8fc2ec3cba61b0671162c045e396552d6b9
ars1810 <122894254+ars1810@users.noreply.github.com>
2025-04-29 13:45:10 +0700

    Update database.go

--- backend/config/database.go
+++ backend/config/database.go
@@ -1,21 +1,33 @@
 package config
 
 import (
-    "gorm.io/gorm"
-    "gorm.io/driver/postgres" // Import driver PostgreSQL
-    "log"
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-    var err error
+	// Ambil variable environment untuk koneksi database
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"), // Ambil host dari environment
+		os.Getenv("DB_USER"), // Ambil user dari environment
+		os.Getenv("DB_PASSWORD"), // Ambil password dari environment
+		os.Getenv("DB_NAME"), // Ambil nama database dari environment
+		os.Getenv("DB_PORT"), // Ambil port dari environment
+	)
 
-    dsn := fmt.Sprintf(
-  "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-  os.Getenv("DB_HOST"),
-  os.Getenv("DB_USER"),
-  os.Getenv("DB_PASSWORD"),
-  os.Getenv("DB_NAME"),
-  os.Getenv("DB_PORT"),
-)
+	var err error
+	// Inisialisasi koneksi ke database
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)  // Menangani error saat gagal konek
+	} else {
+		log.Println("Database connected!")
+	}
+}
